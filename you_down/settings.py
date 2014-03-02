64c55e8e922d1f5dce309e7ecf48cdaee32a4a49
Josh Zheng <denial@gmail.com>
2014-03-02 18:16:50 -0500

    Config database for heroku.

--- you_down/settings.py
+++ you_down/settings.py
@@ -1,8 +1,11 @@
+import os
 
 DEBUG = True
 SECRET_KEY = 'temporary_secret_key' # make sure to change this
-SQLALCHEMY_DATABASE_URI = 'postgres:///youdown'
-#SERVER_NAME = 'ec2-54-82-63-177.compute-1.amazonaws.com'
+
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 ACCOUNT_SID = "ACd89ad7724ff0516b6d980fa1e198c678" 
 AUTH_TOKEN = "84745c9096a1f71d230627784192afe9" 
-HOST_DOMAIN = "ec2-54-82-63-177.compute-1.amazonaws.com"
+
+HOST_DOMAIN = "youdown.herokuapp.com"
